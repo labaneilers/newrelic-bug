815d500d93be4df6c8eeb79751b3c45bf72d8fde
Laban Eilers <laban@eilers.com>
2018-08-21 10:30:46 -0400

    still reproducible

--- server.js
+++ server.js
@@ -5,13 +5,9 @@ const express = require('express');
 const fs = require('fs');
 const newrelic = process.env.NEWRELIC_LICENSE_KEY ? require('newrelic') : null;
 
-// Modules
-const errorHandling = require('./error-handling');
-
 // Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
-const TEMP_DIR = process.env.IMGSRV_TEMP || (__dirname + '/tmp');
 
 // App
 const app = express();
@@ -19,7 +15,7 @@ const app = express();
 const util = require('util');
 let asyncTimeout = util.promisify(setTimeout);
 
-app.get('/success', async (req, res, next) => {
+app.get('/', async (req, res, next) => {
 
   if (newrelic) {
     newrelic.setTransactionName('GET/success');
@@ -38,13 +34,6 @@ app.get('/success', async (req, res, next) => {
     .send('<html><head><title>Success</title></head><body><pre>Success</pre></body></html>');
 });
 
-// Error handling goes last
-app.use(errorHandling.middleware);
 app.listen(PORT, HOST);
-
-// Ensure there's a temp dir
-if (!fs.existsSync(TEMP_DIR)) {
-  fs.mkdirSync(TEMP_DIR);
-}
 
 console.log(`http://${HOST}:${PORT}`);
